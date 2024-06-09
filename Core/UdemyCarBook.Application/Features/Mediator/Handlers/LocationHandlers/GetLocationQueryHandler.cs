1da5c64e865f22de4e2050e20952dc1aefcd5091
Berat Muşdal <beratmsdl@gmail.com>
2024-06-09 20:21:25 +0300

    commit all

---
+++ Core/UdemyCarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyCarBook.Application.Features.Mediator.Queries.LocationQueries;
+using UdemyCarBook.Application.Features.Mediator.Results.FeatureResult;
+using UdemyCarBook.Application.Features.Mediator.Results.LocationResults;
+using UdemyCarBook.Application.Interfaces;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.Application.Features.Mediator.Handlers.LocationHandlers
+{
+    public class GetLServiceQueryHandler : IRequestHandler<GetLocationQuery, List<GetLocationQueryResult>>
+    {
+        private readonly IRepository<Location> _repository;
+
+        public GetLServiceQueryHandler(IRepository<Location> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetLocationQueryResult>> Handle(GetLocationQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(x => new GetLocationQueryResult
+            {
+                LocationID = x.LocationID,
+                Name = x.Name,
+
+            }).ToList();
+        }
+    }
+}
