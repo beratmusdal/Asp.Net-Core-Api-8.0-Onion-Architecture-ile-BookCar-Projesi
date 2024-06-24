2cb24cbfbac49c41e084c6a79ee130ec7f59dd2e
Berat Muşdal <beratmsdl@gmail.com>
2024-06-24 21:05:01 +0300

    24.06.2024 21:04 Update

---
+++ Frontends/UdemyCarBook.WebUI/Controllers/AdminCarController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UdemyCarBook.Dto.CarDtos;
+
+namespace UdemyCarBook.WebUI.Controllers
+{
+    public class AdminCarController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminCarController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult>  Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7216/api/Cars/GetCarWithBrand");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandDtos>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Presentation/UdemyCarBook.WebApi/Controllers/CommentsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UdemyCarBook.Application.Features.RepositoryPattern;
+using UdemyCarBook.Domain.Entities;
+
+namespace UdemyCarBook.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly IGenericRepository<Comment> _commentsRepository;
+
+        public CommentsController(IGenericRepository<Comment> commentsRepository)
+        {
+            _commentsRepository = commentsRepository;
+        }
+        [HttpGet]
+        public IActionResult CommentList() 
+        { 
+            var values = _commentsRepository.GetAll();
+            return Ok(values);
+        }
+        [HttpDelete]
+        public IActionResult RemoveComment(int id)
+        {
+            var value = _commentsRepository.GetById(id);
+            _commentsRepository.Remove(value);
+            return Ok("Yorum Başarıyla Silindi");
+        }
+        [HttpPut]
+        public IActionResult UpdateComment(Comment comment)
+        {
+            _commentsRepository.Update(comment);
+            return Ok("Yorum Başarıyla Güncellendi");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var value = _commentsRepository.GetById(id);
+            return Ok(value);
+        }
+    }
+}
